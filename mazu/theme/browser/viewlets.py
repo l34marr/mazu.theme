204117e02086ca6241846785c17b0786e14fdbb5
TsungWei Hu <marr.tw@gmail.com>
2014-10-08 23:25:30 +0800

    Update Resource Settings.

--- mazu/theme/browser/viewlets.py
+++ mazu/theme/browser/viewlets.py
@@ -4,5 +4,5 @@ from plone.app.layout.viewlets.common import LogoViewlet
 class LogoViewlet(LogoViewlet):
     def update(self):
         super(LogoViewlet, self).update()
-        self.logo_tag = '<img src="' + self.navigation_root_url + '/++theme++mazu/logo.jpg" alt="MaZu Site Logo">'
+        self.logo_tag = '<img src="' + self.navigation_root_url + '/++theme++mazu.theme/logo.jpg" alt="MaZu Site Logo">'
 
--- mazu/theme/interfaces.py
+++ mazu/theme/interfaces.py
@@ -2,6 +2,6 @@ from zope.interface import Interface
 
 
 class ICustomTheme(Interface):
-    """Marker Interface that Defines a Zope 3 Browser Layer for MaZu Project.
+    """Marker Interface that Defines a Zope 3 Browser Layer.
     """
 
